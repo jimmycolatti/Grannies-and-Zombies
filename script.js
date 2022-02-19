f3a08915a833e85316371e2ffe611704f925e961
Jimmy Colatti <jimmyrcolatti@gmail.com>
2022-02-19 18:00:35 -0500

    floating messages and more features

--- script.js
+++ script.js
@@ -145,20 +145,7 @@ class Defender {
     }
   }
 }
-canvas.addEventListener('click', function () {
-  const gridPositionX = mouse.x - (mouse.x % cellSize) + cellGap;
-  const gridPositionY = mouse.y - (mouse.y % cellSize) + cellGap;
-  if (gridPositionY < cellSize) return;
-  for (let i = 0; i < defenders.length; i++) {
-    if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY)
-      return;
-  }
-  let defenderCost = 100;
-  if (numberOfResources >= defenderCost) {
-    defenders.push(new Defender(gridPositionX, gridPositionY));
-    numberOfResources -= defenderCost;
-  }
-});
+
 function handleDefenders() {
   for (let i = 0; i < defenders.length; i++) {
     defenders[i].draw();
@@ -178,6 +165,43 @@ function handleDefenders() {
         i--;
         enemies[j].movement = enemies[j].speed;
       }
+    }
+  }
+}
+
+// Floating Messages
+const floatingMessages = [];
+
+class FloatingMessage {
+  constructor(value, x, y, size, color) {
+    this.value = value;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.lifeSpan = 0;
+    this.color = color;
+    this.opacity = 1;
+  }
+  update() {
+    this.y -= 0.3;
+    this.lifeSpan += 1;
+    if (this.opacity > 0.03) this.opacity -= 0.03;
+  }
+  draw() {
+    ctx.globalAlpha = this.opacity;
+    ctx.fillStyle = this.color;
+    ctx.font = this.size + 'px Orbitron';
+    ctx.fillText(this.value, this.x, this.y);
+    ctx.globalAlpha = 1;
+  }
+}
+function handleFloatingMessages() {
+  for (let i = 0; i < floatingMessages.length; i++) {
+    floatingMessages[i].update();
+    floatingMessages[i].draw();
+    if (floatingMessages[i].lifeSpan >= 50) {
+      floatingMessages.splice(i, 1);
+      i--;
     }
   }
 }
@@ -214,6 +238,18 @@ function handleEnemies() {
     }
     if (enemies[i].health <= 0) {
       let gainedResources = enemies[i].maxHealth / 10;
+      floatingMessages.push(
+        new FloatingMessage(
+          '+' + gainedResources,
+          enemies[i].x,
+          enemies[i].y,
+          30,
+          'black'
+        )
+      );
+      floatingMessages.push(
+        new FloatingMessage('+' + gainedResources, 250, 50, 30, 'gold')
+      );
       numberOfResources += gainedResources;
       score += gainedResources;
       const findThisIndex = enemyPositions.indexOf(enemies[i].y);
@@ -259,6 +295,18 @@ function handleResources() {
     resources[i].draw();
     if (resources[i] && mouse.x && mouse.y && collision(resources[i], mouse)) {
       numberOfResources += resources[i].amount;
+      floatingMessages.push(
+        new FloatingMessage(
+          '+' + resources[i].amount,
+          resources[i].x,
+          resources[i].y,
+          30,
+          'black'
+        )
+      );
+      floatingMessages.push(
+        new FloatingMessage('+' + resources[i].amount, 250, 50, 30, 'gold')
+      );
       resources.splice(i, 1);
       i--;
     }
@@ -285,6 +333,25 @@ function handleGameStatus() {
   }
 }
 
+canvas.addEventListener('click', function () {
+  const gridPositionX = mouse.x - (mouse.x % cellSize) + cellGap;
+  const gridPositionY = mouse.y - (mouse.y % cellSize) + cellGap;
+  if (gridPositionY < cellSize) return;
+  for (let i = 0; i < defenders.length; i++) {
+    if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY)
+      return;
+  }
+  let defenderCost = 100;
+  if (numberOfResources >= defenderCost) {
+    defenders.push(new Defender(gridPositionX, gridPositionY));
+    numberOfResources -= defenderCost;
+  } else {
+    floatingMessages.push(
+      new FloatingMessage('need more resources', mouse.x, mouse.y, 20, 'blue')
+    );
+  }
+});
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'blue';
@@ -295,6 +362,7 @@ function animate() {
   handleProjectiles();
   handleEnemies();
   handleGameStatus();
+  handleFloatingMessages();
   frame++;
   if (!gameOver) requestAnimationFrame(animate);
 }
