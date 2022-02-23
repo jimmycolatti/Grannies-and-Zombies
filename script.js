a7ec739e5b98d1fe65b8d3c86a94222d8670cd6d
Jimmy Colatti <jimmyrcolatti@gmail.com>
2022-02-22 21:59:42 -0500

    added sprites

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ function handleGameGrid() {
   }
 }
 // projectiles
+
 class Projectile {
   constructor(x, y) {
     this.x = x;
@@ -116,6 +117,9 @@ function handleProjectiles() {
 }
 
 // defenders
+const defender1 = new Image();
+defender1.src = './sprites/oldlady1_idle.png';
+
 class Defender {
   constructor(x, y) {
     this.x = x;
@@ -126,13 +130,30 @@ class Defender {
     this.health = 100;
     this.projectiles = [];
     this.timer = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.spriteWidth = 1143;
+    this.spriteHeight = 994;
+    this.minFrame = 0;
+    this.maxFrame = 19;
   }
   draw() {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
     ctx.fillStyle = 'gold';
     ctx.font = '30px Orbitron';
     ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+    ctx.drawImage(
+      defender1,
+      this.frameX * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
   update() {
     if (this.shooting) {
@@ -207,6 +228,14 @@ function handleFloatingMessages() {
 }
 
 // enemies
+const enemyTypes = [];
+const enemy1 = new Image();
+enemy1.src = './sprites/zombie1.png';
+enemyTypes.push(enemy1);
+const enemy2 = new Image();
+enemy2.src = './sprites/zombie2.png';
+enemyTypes.push(enemy2);
+
 class Enemy {
   constructor(verticalPosition) {
     this.x = canvas.width;
@@ -217,16 +246,39 @@ class Enemy {
     this.movement = this.speed;
     this.health = 100;
     this.maxHealth = this.health;
+    this.enemyType = enemyTypes[Math.floor(Math.random() * enemyTypes.length)];
+    this.frameX = 0;
+    this.frameY = 0;
+    this.minFrame = 0;
+    this.maxFrame = 3;
+    this.spriteWidth = 972;
+    this.spriteHeight = 818;
   }
   update() {
     this.x -= this.movement;
+    if (frame % 20 === 0) {
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = this.minFrame;
+    }
   }
   draw() {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
     ctx.fillStyle = 'black';
     ctx.font = '30px Orbitron';
     ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+    // ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(
+      this.enemyType,
+      this.frameX * this.spriteWidth,
+      this.frameY * this.spriteHeight,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 function handleEnemies() {
