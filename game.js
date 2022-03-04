c67e3b3cb35fe3a10b40fb668921182ae7dd492d
Jimmy Colatti <jimmyrcolatti@gmail.com>
2022-03-03 20:24:52 -0500

    changed winningScore

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ let enemiesInterval = 800;
 let frame = 0;
 let gameOver = false;
 let score = 0;
-const winningScore = 50;
+const winningScore = 30;
 let chosenDefender = 1;
 
 const gameGrid = [];
